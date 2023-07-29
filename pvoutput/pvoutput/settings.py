2404993b9e7fda27d59c86cf365588a91e0fb074
Nnaobi <nnaobi.godson@aiesec.net>
2023-07-29 15:33:35 +0100

    added countries from pvoutput

--- pvoutput/pvoutput/settings.py
+++ pvoutput/pvoutput/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "pvoutput.spiders"
 #USER_AGENT = "pvoutput (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- pvoutput/pvoutput/spiders/pvoutput_spiders.py
+++ pvoutput/pvoutput/spiders/pvoutput_spiders.py
@@ -1,19 +1,49 @@
+import re
+
 import scrapy
 
 class CountrySpider(scrapy.Spider):
-    name = "month"
+    name = "country"
     start_urls = [
+        #f"https://pvoutput.org/ladder.jsp?f=1&country={self.id}"
         "https://pvoutput.org/ladder.jsp?f=1&country=257"
     ]
 
-    def __init__(self, url):
+    def __init__(self, url=None, id=None):
         self.url = url
+        self.id = id
 
 
     #get all the urls and check if the country is the same with the expected
 
-    def parse_
+    def parse(self, response):
+        table_rows = response.css(".e2, .o2")
+        sids = []
+        for row in table_rows:
+            tag = row.css("a:first-child")
+            link = tag.xpath("@href").get()
+            result = re.search(r"sid=(\d+)", link)
+            if result:
+                result = result.group(1)
 
+class GetCountries(scrapy.Spider):
+    name = "state"
+    start_urls = [
+        f"https://pvoutput.org/ladder.jsp?f=1&country={i}" for i in range(257)
+    ]
+
+    def parse(self, response):
+        try:
+            url = response.request.url
+            country_id = re.search(r"country=(\d+)", url)
+            if country_id:
+                country_id = country_id.group(1)
+
+            table_rows = response.css(".e2, .o2")
+            country = table_rows[0].xpath("//td[4]//text()").get()
+            print(f"{country.strip()}:{country_id}")
+        except IndexError:
+            raise
 
 class WeekSpider(scrapy.Spider):
     def __init__(self, url):
