f76ed108648f3cd9b71da3297b903b86cb8f6782
Nnaobi <nnaobi.godson@aiesec.net>
2023-08-01 00:53:39 +0100

    added location info scraping

--- pvoutput/pvoutput/spiders/pvoutput_spiders.py
+++ pvoutput/pvoutput/spiders/pvoutput_spiders.py
@@ -8,7 +8,7 @@ from ..definitions import SCRAPPER_ROOT_DIR
 
 class CountrySpider(scrapy.Spider):
     """
-    Gets all the system urls for a country
+    Gets all the system ids for a country
     """
     name = "country"
 
@@ -25,26 +25,27 @@ class CountrySpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        next_link = response.xpath("//a[contains(text(), 'Next')]")
-        if next_link:
-            next_href = next_link[0].attrib["href"]
-            next_href = f"https://pvoutput.org/ladder.jsp{next_href}"
-            yield scrapy.Request(next_href, self.parse_system_ids)
-
-    def parse_system_ids(self, response):
+        system_ids = []
         table_rows = response.css(".e2, .o2")
-        sids = []
         for row in table_rows:
             country = row.xpath(".//td[4]//text()").get()
-            if country != "Switzerland": #self.country:
+            if country.strip().lower() != "switzerland": #self.country:
                 continue
-            print("na so")
+
             tag = row.css("a:first-child")
             link = tag.xpath("@href").get()
             system_id = re.search(r"sid=(\d+)", link)
             if system_id:
-                system_id = system_id.group(1)
-                with open("out.txt", "a")as text_file:
+                system_ids.append(system_id.group(1))
+        
+        next_link = response.xpath("//a[contains(text(), 'Next')]")
+        if next_link:
+            next_href = next_link[0].attrib["href"]
+            next_href = f"https://pvoutput.org/ladder.jsp{next_href}"
+            yield scrapy.Request(next_href, self.parse)
+        else:
+            with open("out.txt", "w")as text_file:
+                for system_id in system_ids:
                     text_file.write(system_id + "\n")
 
 
@@ -80,15 +81,28 @@ class GetCountries(scrapy.Spider):
                 out_file.write(f"{country.strip()}:{country_id}\n")
 
 
-class WeekSpider(scrapy.Spider):
-    def __init__(self, url):
-        self.url = url
+class SystemLocation(scrapy.Spider):
+    # def __init__(self, url):
+    #     self.url = url
 
-    name = "month"
+    name = "system_location_spider"
     start_urls = [
-        "https://pvoutput.org/ladder.jsp?f=1&country=257"
+        "https://pvoutput.org/listmap.jsp?sid=34873"
     ]
     
+    def parse(self, response):
+        location = {}
+        script_map_info = response.xpath(
+            "//script[contains(., 'var mymap = L.map')]").get()
+        location_info = re.search(
+            r"center:\s*\[([\d.-]+),\s*([\d.-]+)\]", script_map_info
+        )
+        if location_info:
+            location["latitude"] = location_info.group(1)
+            location["longitude"] = location_info.group(2)
+        print(location)
+
+
 
 class DaySpider(scrapy.Spider):
     def __init__(self, url):
